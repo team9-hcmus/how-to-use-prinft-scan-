4733217b54d0dad850b500777492fd62f4c8370e
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-07 13:29:40 +0700

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+    printf("enter a");
+    scanf_s("%d", &a);
+    printf("enter b");
+    scanf_s("%d", &b);
+    printf("enter c");
+    scanf_s("%d", &c);
+    printf("%d,%d,%d", a, b, c);
+    return(0);
+}
